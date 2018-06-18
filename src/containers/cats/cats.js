865136fdd89bee7842fdb941dc9bfc2dcba85fa9
holly.mae.rogers@gmail.com <holly.mae.rogers@gmail.com>
2018-06-18 11:13:16 +0100

    changes to route

--- src/containers/cats/cats.js
+++ src/containers/cats/cats.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-<<<<<<< HEAD
+
 import RandomCat from 'components/cats/cat'
 
 class RandomCatContainer extends Component {
   render() {
     return <RandomCat onRandomCat={() => this.props.dispatch(onRandomCat())} currentCat={this.props.cats} />
-=======
-import RandomCat from 'components/cats'
-
-class RandomCatContainer extends Component {
-  render() {
-    return <RandomCat onRandomCat={() => this.props.dispatch(onRandomCat())} currentCat={this.props.cat} />
->>>>>>> 0aa5dc35c1453463dd4cd27bb5bd50f6e40d3414
   }
 }
 
